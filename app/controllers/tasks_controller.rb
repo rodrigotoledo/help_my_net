a86ad586e307f87c07b39415cc94260fb18d6ee1
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-11 20:29:27 -0300

    system of has and belongs to many employees into the tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def task_params
-    params.require(:task).permit(:category_id, :title, :description, :completed_at)
+    params.require(:task).permit(:category_id, :title, :description, :completed_at, :employee_ids => [])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,5 @@ class Task < ApplicationRecord
   belongs_to :category, optional: false
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
+  has_and_belongs_to_many :employees
 end
