acb2eda01f88716c291124658200c3e3be1a9ece
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-12 17:10:51 -0300

    coordinates of employees and using papertrail

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ApplicationRecord
   has_paper_trail
-  validates :name, :location, :document, :email, :alternative_email, :mobile_number, :phone_number, :address, presence: true
+  scope :actives, -> { where.not(latitude: nil, longitude: nil) }
+  validates :name, :document, :email, :alternative_email, :mobile_number, :phone_number, :address, presence: true
   validates :email, :document, uniqueness: true
   after_validation :geocode
-  geocoded_by :location
+  geocoded_by :address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_140446) do
+ActiveRecord::Schema.define(version: 2021_12_12_140251) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2021_12_12_140446) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
-    t.string "location"
   end
 
   create_table "employees_tasks", id: false, force: :cascade do |t|
