7b32da15559f44a8ceb3cd26ca89771b8f6fe785
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-31 21:46:44 -0300

    heroku fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
