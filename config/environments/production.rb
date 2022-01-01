0954817eaa7424477377b2356e167559e7e1852b
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-31 22:22:24 -0300

    hotfix production to allowed requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,5 +99,6 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.web_socket_server_url = "wss://help-my-net-ror7.herokuapp.com/cable" 
+  config.web_socket_server_url = "wss://help-my-net-ror7.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ 'https://help-my-net-ror7.herokuapp.com', /http:\/\/help-my-net-ror7.herokuapp.com.*/ ]
 end
