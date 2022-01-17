a7300307596bba800b921af459006901a8b75389
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2022-01-17 10:49:52 -0300

    real-time operations with employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,7 +5,7 @@ class Employee < ApplicationRecord
          :recoverable, :rememberable, :validatable
   after_validation :geocode
   has_many :employee_tasks
-  has_many :tasks, through: :employee_tasks
+  has_many :tasks, through: :employee_tasks, dependent: :destroy
   geocoded_by :address
   validates :name, :address, :document, presence: true
 
@@ -14,7 +14,19 @@ class Employee < ApplicationRecord
   end
   after_update do
     broadcast_update_to "employees"
+    self.tasks.each do |task|
+      task.employees.reload
+      broadcast_update_to "employees", target: "turbo_map_#{task.id}", partial: "tasks/map", locals: {task: task}
+    end
   end
+
+  before_destroy prepend: true do
+    self.tasks.each do |task|
+      task.employees.reload
+      broadcast_update_to "employees", target: "turbo_map_#{task.id}", partial: "tasks/map", locals: {task: task}
+    end
+  end
+
   after_destroy_commit do
     broadcast_remove_to "employees"
   end
