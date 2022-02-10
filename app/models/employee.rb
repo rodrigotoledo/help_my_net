41b18149e52bb5377517389fbee574e14258c705
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2022-02-10 00:56:44 -0300

    removing the route

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,7 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   after_validation :geocode
-  has_many :employee_tasks
+  has_many :employee_tasks, dependent: :destroy
   has_many :tasks, through: :employee_tasks
   geocoded_by :address
   validates :name, :address, :document, presence: true
@@ -18,6 +18,7 @@ class Employee < ApplicationRecord
       broadcast_update_to "tasks", partial: "tasks/task", locals: {task: task}, target: "task_#{task.id}"
     end
   end
+
   after_destroy_commit do
     broadcast_remove_to "employees"
   end
--- app/models/employee_task.rb
+++ app/models/employee_task.rb
@@ -1,4 +1,8 @@
 class EmployeeTask < ApplicationRecord
   belongs_to :employee
   belongs_to :task
+
+  after_destroy_commit do
+    broadcast_update_to "tasks", partial: "tasks/task", locals: {task: task}, target: "task_#{task.id}"
+  end
 end
